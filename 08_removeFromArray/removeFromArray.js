e89a8af86be3b4193ffcbfb8339b9b1f850cfe11
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-20 18:57:10 -0400

    Complete the first exercise of this exercise
    My code is able to delete 1 item from an inputted
    array, which allowed it to pass this exercise.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, item) {
+  if (!array.includes(item)) {
+    return `Error. ${item} does not exist.`
+  }
+
+  const itemIndex = array.indexOf(item);
+
+  array.splice(itemIndex, 1);
+
+  return array;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
