40c4dd04d30724a08a76980e26e83e5e026cfb41
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-19 23:21:41 -0400

    Complete the first test of the exercise
    The code that I wrote in the provided function
    declaration was able to repeat the string "hey" 3
    times. So, my code was able to pass the first
    test.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let repeatedString = "";
+  
+  for (let i = 0; i < num; i++) {
+    repeatedString += string;
+  }
 
+  return repeatedString; 
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
