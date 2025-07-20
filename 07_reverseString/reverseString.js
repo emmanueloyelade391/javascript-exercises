2f2896a8a7ca4e029cab86dd4c5caa569905f8e9
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-20 15:37:11 -0400

    Complete the first test of this exercise
    My code is able to reverse the letters of single
    strings and return a new string with the entire
    string reversed. This allowed my code to pass the
    first test.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,38 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  const arr = string.split(" ");
 
-};
+  const reversedString = arr.map(function(item) {
+    let reversedString = [];
+
+    for (let i = 1; i < item.length + 1; i++) {
+      reversedString += item.at(-(i));
+    }
+
+    return reversedString;
+  });
+
+  const result = reversedString.join(" ");
+
+  return result;
+}
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
+
+/*
+let arr = string.split(" ");
+
+  let result = arr.map(function(item) {
+    let reversedString;
+    for (let i = 1; i < item.length + 1; i++) {
+      reversedString += item.at(-i);
+    } 
+
+    return reversedString;
+    });
+  
+  const stringResult = result.join(" ");
+  return stringResult;
+  */
