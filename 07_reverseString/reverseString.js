5d1e0ef680d2c812f2a576d9d41a928530637492
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-20 16:07:45 -0400

    Complete the second test of the exercise
    My code is able to reverse the order of strings
    with multiple words in them, which allowed it to
    pass the second test.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,7 +1,17 @@
 const reverseString = function(string) {
   const arr = string.split(" ");
 
-  const reversedString = arr.map(function(item) {
+  console.log(arr);
+
+  //I need to create an array that reverses the order of the literal
+  //items before reversing the words of the individual items.
+  let reversedArray = [];
+
+  for (let i = 1; i < arr.length + 1; i++) {
+    reversedArray.push(arr.at(-(i)));
+  }
+
+  const reversedString = reversedArray.map(function(item) {
     let reversedString = [];
 
     for (let i = 1; i < item.length + 1; i++) {
@@ -16,7 +26,7 @@ const reverseString = function(string) {
   return result;
 }
 
-console.log(reverseString("hello"));
+console.log(reverseString("hello there"));
 
 // Do not edit below this line
 module.exports = reverseString;
