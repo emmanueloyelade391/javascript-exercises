2da703b3a1bd2fec8d82c7e125d735c86e3765a3
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-21 13:55:00 -0400

    Complete the second test of this exercise
    My code rounds the conversion of any degree from
    farenheit to celsius to 1 decimal point, which
    allowed it to pass this test.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
 const convertToCelsius = function(temp) {
-  let convertedTemp = (temp - 32) * (5/9);
+  let convertedTemp = +((temp - 32) * (5/9)).toFixed(1);
   return convertedTemp;
 };
 
 const convertToFahrenheit = function() {
 };
 
-console.log(convertToCelsius(32));
+console.log(convertToCelsius(100));
 
 // Do not edit below this line
 module.exports = {
