499f547efcd89f555d4cdbd12611431e5f8c454c
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-21 00:24:16 -0400

    Complete the first test of this exercise
    My code was able to use loops to calculate the sum
    between the range of 2 numbers (including the 2
    numbers themselves). This allowed my code to pass
    this test.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+  let sum = 0;
+  for (let i = x; i < y + 1; i++) {
+    sum += i;
+  }
 
+  return sum;
 };
+
+console.log(sumAll(2, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
