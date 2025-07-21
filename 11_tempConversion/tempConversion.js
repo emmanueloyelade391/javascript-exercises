b050b2fe6025c3f5e1c51d7ec8b929b6f073105c
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-21 13:49:00 -0400

    Complete the first test of this exercise
    My code is able to correctly convert from
    farenheit to celsius, which allowed it to pass
    this test.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convertedTemp = (temp - 32) * (5/9);
+  return convertedTemp;
 };
 
 const convertToFahrenheit = function() {
 };
+
+console.log(convertToCelsius(32));
 
 // Do not edit below this line
 module.exports = {
