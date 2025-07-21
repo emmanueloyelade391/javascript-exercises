31ef5d591d0cd0db7a137260b023517ffea146e0
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-21 00:32:21 -0400

    Complete the third test
    My code is able to handle cases where the first
    inputted number is bigger than the second inputted
    number, which allowed my code to pass this test.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
 const sumAll = function(x, y) {
   let sum = 0;
+
+  if (x > y) {
+    for (let i = y; i < x + 1; i++) {
+      sum += i;
+    }
+  }
+
   for (let i = x; i < y + 1; i++) {
     sum += i;
   }
