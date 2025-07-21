a7226a929a04e4a5845cc25899c487856de1afb9
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-21 12:55:29 -0400

    Complete the fourth test of this exercise
    My code is able to tell that a century year is not
    a leap year, which allowed my code to pass this
    test.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,12 +1,20 @@
 const leapYears = function(year) {
-  if (year % 4 === 0 || year % 400 === 0) {
+  if (year % 100 === 0) {
+    if (year % 400 === 0) {
+      return true;
+    } else {
+    return false;
+  } 
+  }
+
+  if (year % 4 === 0 /*|| year % 400 === 0*/) {
     return true 
   } else {
     return false;
   }
 };
 
-console.log(leapYears(34992));
+console.log(leapYears(1900));
 
 // Do not edit below this line
 module.exports = leapYears;
