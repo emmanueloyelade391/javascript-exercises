a66efd557a9a05d5baac7a57c1b23ab77de38b8b
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-21 14:51:49 -0400

    Complete the third test of this exercise
    My code is able to remove an item that appears
    multiple times in the array that is inputted,
    which allowed it to pass this test.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,18 +1,13 @@
 const removeFromArray = function(array, ...items) {
-  for (let i = 0; i < items.length; i++){
-    if (!array.includes(items[i])) {
-    return `Error. ${items[i]} does not exist.`
+  const resultArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (!(items.includes(array[i]))) {
+      resultArray.push(array[i]);
     }
   }
- 
-  for (let i = 0; i < items.length; i++) {
-    let itemIndex = array.indexOf(items[i]);
 
-    array.splice(itemIndex, 1);
-  }
-  
-
-  return array;
+  return resultArray;
 };
 
 console.log(removeFromArray([1, 2, 2, 3], 2)); //Continue working on this exercise tomorrow
