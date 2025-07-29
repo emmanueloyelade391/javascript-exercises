cf90687f9025962c861d7acca7e44ea92642c391
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-29 19:57:18 -0400

    Begin the fifteenth test of this exercise
    I deleted the .skip extension of the fifteenth
    test to start working on it.

--- 12_calculator/calculator.spec.js
+++ 12_calculator/calculator.spec.js
@@ -67,7 +67,7 @@ describe('power', () => {
 });
 
 describe('factorial', () => {
-  test.skip('computes the factorial of 0', () => {
+  test('computes the factorial of 0', () => {
     expect(calculator.factorial(0)).toBe(1); // 0! = 1
   });
 
