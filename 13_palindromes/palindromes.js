cba4b70d11d417253b9e171b527729f0af5faa7d
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-07-29 21:47:06 -0400

    Complete the first test of this exercise
    My code is able to correctly determine that
    racecar is a palindrome, which allowed it to pass
    this test.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let front = string[0];
+  let end = string.at(-1);
+  for (let i = 0; i < string.length; i++) {
+    if (front !== end) {
+      return false;
+    }
+    front = string[i];
+    console.log(front);
+    end = string.at(-(i + 1));
+    console.log(end);
+  }
+  return true; 
 };
+
+console.log(palindromes("racecar"));
 
 // Do not edit below this line
 module.exports = palindromes;
