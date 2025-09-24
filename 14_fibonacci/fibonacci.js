87ee1a933f6749765769ee2b4b857734115aca71
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-09-24 17:36:01 -0400

    Complete the sixth test of this exercise
    My code correctly handles the case where the
    input is 0 by returning 0, which allowed it pass
    this test and move on to the next one.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
 const fibonacci = function(num) {
+  if (num == 0) {
+    return 0;
+  }
+  
+  if (num == 1) {
+    return 1;
+  }
+
   let fibonacciNums = []
   fibonacciNums[0] = 0;
   fibonacciNums[1] = 1;
@@ -10,7 +18,7 @@ const fibonacci = function(num) {
   return fibonacciNums[num];
 };
 
-console.log(fibonacci(25));
+console.log(fibonacci(0));
 
 // Do not edit below this line
 module.exports = fibonacci;
