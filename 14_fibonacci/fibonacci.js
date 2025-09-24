711d2a57ef41c35513bdf583f12af72c6121de9a
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-09-24 18:07:09 -0400

    Complete the eighth test of this exercise
    My code correctly accepts strings as input and
    computes the correct fibonacci number, which
    allowed it to pass this test and move on to the
    next one.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,13 +1,15 @@
 const fibonacci = function(num) {
-  if (num < 0) {
+  let newNum = +num;
+
+  if (newNum < 0) {
     return "OOPS";
   }
   
-  if (num == 0) {
+  if (newNum == 0) {
     return 0;
   }
   
-  if (num == 1) {
+  if (newNum == 1) {
     return 1;
   }
 
@@ -15,14 +17,14 @@ const fibonacci = function(num) {
   fibonacciNums[0] = 0;
   fibonacciNums[1] = 1;
 
-  for (let i = 2; i < num + 1; i++) {
+  for (let i = 2; i < newNum + 1; i++) {
     fibonacciNums[i] = fibonacciNums[i - 1] + fibonacciNums[i - 2];
   }
 
-  return fibonacciNums[num];
+  return fibonacciNums[newNum];
 };
 
-console.log(fibonacci(-25));
+console.log(fibonacci("0"));
 
 // Do not edit below this line
 module.exports = fibonacci;
