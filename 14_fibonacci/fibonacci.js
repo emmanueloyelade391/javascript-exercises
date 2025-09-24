a4f9004545793e0b4a7da9905b9399ae65f2d729
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-09-24 16:54:38 -0400

    Complete the first test of this exercise
    My code is able to compute the 4th fibonacci
    number, which allowed it to pass this test and
    move on to the next one.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let fibonacciNums = []
+  fibonacciNums[0] = 0;
+  fibonacciNums[1] = 1;
 
+  for (let i = 2; i < num + 1; i++) {
+    fibonacciNums[i] = fibonacciNums[i - 1] + fibonacciNums[i - 2];
+  }
+
+  return fibonacciNums[num]
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
