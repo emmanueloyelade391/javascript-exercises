02ce1521f772619f19fdee3b6753d8cf4c1bc52a
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-10-02 02:23:50 -0400

    Complete the exercise
    My code is able to return an array of titles using
    an array of objects containing titles.

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+let results = []
+for (let item of array) {
+  let title = item.title;
+  results.push(title);
+}
 
+return results;
 };
+
+const books = [
+  {
+    title: "Book", 
+    author: "Name"
+  },
+  {
+    title: "Book2",
+    author: "Name2"
+  }
+];
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
