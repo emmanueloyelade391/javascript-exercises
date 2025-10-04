a27628fa2974c85c94457bf52f52e03155ee7c65
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-10-04 09:49:07 -0400

    Complete the first test of this exercise
    My code is able to find the person with the
    greatest age in an array of objects that contain
    the years of birth and death, which allows it to
    move on to the next test.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  array.sort((a, b) =>(a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth));
+  return array.at(-1);
 };
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
